f7d79bf6dbb75cf9cff1a7f65b288a75bb548833
Jhonatan <jhonatan.sampaiof@gmail.com>
2024-03-17 16:31:26 -0300

    Beverages adicionados

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import Hamburgers from "./pages/Hamburgers/Hamburgers";
 import Appetizers from "./pages/Appetizers/Appetizers";
 import Desserts from "./pages/Desserts/Desserts";
+import Beverages from "./pages/Beverages/Beverages";
 
 
 import Login from "./pages/Login/Login";
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/entradinhas" element={<Appetizers />} />
         <Route path="/hamburgers" element={<Hamburgers />} />
         <Route path="/sobremesas" element={<Desserts />} />
+        <Route path="/bebidas" element={<Beverages />} />
         
         <Route path="/login" element={<Login />} />
       </Routes>
