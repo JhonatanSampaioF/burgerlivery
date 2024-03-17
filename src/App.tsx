952c0ec44f1bfac558e0e5d50cc05913bed303bb
Jhonatan <jhonatan.sampaiof@gmail.com>
2024-03-17 16:18:43 -0300

    Appetizers adicionados

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Hamburgers from "./pages/Hamburgers/Hamburgers";
 import Appetizers from "./pages/Appetizers/Appetizers";
+
+
 import Login from "./pages/Login/Login";
 
 export default function App() {
@@ -11,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/entradinhas" element={<Appetizers />} />
         <Route path="/hamburgers" element={<Hamburgers />} />
+        
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Appetizers/Appetizers.tsx
+++ src/pages/Appetizers/Appetizers.tsx
@@ -1,10 +1,24 @@
 import { useEffect, useState } from "react";
-import { CategoryList, Layout } from "../../components";
-import { ProductCategories } from "../Hamburgers/Hamburgers.style";
+import { Button, CategoryList, Layout, ProductCard } from "../../components";
+import { ProductCategories, ProductWrapper } from "../Hamburgers/Hamburgers.style";
+import {
+  ProductCardContent,
+  ProductCardPrice,
+} from "../../components/ProductCard/ProductCard.style";
 
 export default function Appetizers() {
   const [isLoading, setIsLoading] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const priceFormat = (price: number) => {
+    console.log("cai aqui?");
+
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  };
 
   const getCategories = async () => {
     const url = "http://localhost:8000/categories";
@@ -22,13 +36,45 @@ export default function Appetizers() {
     }
   };
 
+  const getAppetizers = async () => {
+    const url = "http://localhost:8000/appetizers";
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    getCategories();
+    if (categories.length) {
+      return;
+    }
+
+    return () => {
+      getCategories();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (products.length) {
+      return;
+    }
+
+    return () => {
+      getAppetizers();
+    };
   }, []);
 
   return (
     <Layout>
-      <h1>Entradinhas</h1>
+      <h1>Hamburgers</h1>
       <ProductCategories>
         {isLoading ? (
           <p>Carregando</p>
@@ -38,6 +84,25 @@ export default function Appetizers() {
           ))
         )}
       </ProductCategories>
+      <ProductWrapper>
+        {isLoading ? (
+          <p>Carregando</p>
+        ) : (
+          products.map((product, index) => (
+            <ProductCard key={index}>
+              <ProductCardContent>
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <Button onClick={() => {}}>Adicionar</Button>
+              </ProductCardContent>
+              <ProductCardPrice>
+                {priceFormat(product.values)}
+              </ProductCardPrice>
+              <img src={product.image} alt={product.title} />
+            </ProductCard>
+          ))
+        )}
+      </ProductWrapper>
     </Layout>
   );
 }
